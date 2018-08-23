94343cc2ce6387fa646d326b813bfb4b2713437c
chengxiaolv <972987164@qq.com>
2018-08-23 17:44:40 +0800

    添加金钱格式的数字

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,6 +1,7 @@
 // 全局注册filters参考地址   https://blog.csdn.net/qq_33207292/article/details/78562314
 // 使用方法  {{xxx  |  过滤器名称}}
 // 首字母大写 uppercase
+// 转化为 12,345,345.845金钱的格式   TransformationNumber
 
 
 // 首字母大写 uppercase
@@ -10,4 +11,12 @@ export function firstOneUppercase(value) {
     }
     value = value.toString();
     return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
+// 转化为 12,345,345.845  金钱的格式。
+export function TransformationNumber(vaule) {
+    vaule = vaule.toString();
+    var num = vaule;
+    num = num.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    return num;
 }
